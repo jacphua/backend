7eb0e9f8a6f9ebdc9e0ec17727c0933d576a9859
Jac Phua <jac@witgritfit.com>
2022-08-13 16:03:15 +0300

    update database.js file

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 
 
-var connection = new sqlite3.Database('./market.db', (err) => {
+var connection = new sqlite3.Database('./shopping.db', (err) => {
   if (err) {
     console.error(err.message);
   }
-  console.log('Connected to the market database.');
+  console.log('Connected to the shopping database.');
 });
 
 module.exports = { connection };
