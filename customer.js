293ffd7f863a97c9a236b2fc22d37647e8899b72
Jac Phua <jac@witgritfit.com>
2022-08-31 03:06:09 +0300

    31 Aug update

--- customer.js
+++ customer.js
@@ -1,5 +1,5 @@
 // This file will contain the queries to the customer table
-const database = require("./database");
+const database = require("./shopping.db");
 const express = require("express");
 
 // Allows us to define a mapping from the URI to a function
@@ -7,20 +7,39 @@ router = express.Router();
 
 // can be used to define a GET API.   URI -> CB function.
 router.get("/customer/all", (request, response) => {
-  database.connection.all("select * from customer", (errors, results) => {
+  sqlst = 'select * from customer',
+  database.connection.all (
+    sqlst,
+    (errors, results) => {
     if (errors) {
       response.status(500).send("Some error occurred");
     } else {
       response.status(200).send(results);
     }
-  });
+  }
+  )
 });
-//Note: use query instead of all for MySQL - database.connection.query("select * from customer"
+//Note: use query instead of all for MySQL - database.connection.query("select * from customer")
 
 // defines an API which takes id in the request and return the record in response
 router.get("/customer/id", (request, response) => {
   database.connection.all(
     `select * from customer where customer_id = ${request.query.cid}`,
+    (errors, results) => {
+      if (errors) {
+        response.status(500).send("Some error occurred");
+      } else {
+        response.status(200).send(results);
+      }
+    }
+  );
+});
+
+// CHECK - defines an API which takes id & password in the request and return the record in response
+router.get("/customer/login", (request, response) => {
+  sqlst = 'select * from customer where customer_id = ${request.query.cid} & password = ${request.query.pwd}',
+  database.connection.all(
+    sqlst,
     (errors, results) => {
       if (errors) {
         response.status(500).send("Some error occurred");
